5490a9495347111147f0d300fddcfbfea8676f24
Maxence Dunnewind <maxence@typhon.com>
2014-10-29 16:41:07 +0100

    fix variable lookup

--- manifests/config/item.pp
+++ manifests/config/item.pp
@@ -7,8 +7,8 @@
 # Maxence Dunnewind <tech@typhon.com>
 #
 define pureftpd::config::item (){
-  if inline_template('<%= scope.lookupvar("::pureftpd::safe_config[@title.downcase]") %>') != 'undef' {
-    notice inline_template('<%= scope.lookupvar("::pureftpd::safe_config[@title.downcase]") %>') 
+  if inline_template('<%= scope.lookupvar("::pureftpd::safe_config")[@title.downcase] %>') != 'undef' {
+    notice inline_template('<%= scope.lookupvar("::pureftpd::safe_config")[@title.downcase] %>')
     file { "${pureftpd::params::conf_dir}/${title}":
       ensure  => file,
       content => template("${module_name}/${pureftpd::params::conf_erb}"),
