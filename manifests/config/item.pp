a2bb603b291b2a3b0a235bca2f7aac216df1ce9e
Maxence Dunnewind <maxence@typhon.com>
2014-10-29 14:03:06 +0100

    pureftp::config::item is a define, not a class

--- manifests/config/item.pp
+++ manifests/config/item.pp
@@ -1,4 +1,4 @@
-# == Class: pureftpd::config::item
+# == Define pureftpd::config::item
 #
 # Full description of class pureftpd::config::item here
 #
@@ -6,7 +6,7 @@
 #
 # Maxence Dunnewind <tech@typhon.com>
 #
-class pureftpd::config::item (){
+define pureftpd::config::item (){
   if inline_template('<%= scope.lookupvar(@title.downcase) %>') != 'undef' {
     file { "${pureftpd::params::conf_path}/${title}":
       ensure  => file,
